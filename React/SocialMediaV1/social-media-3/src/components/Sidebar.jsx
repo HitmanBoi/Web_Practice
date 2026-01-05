b0b4234df9d814af62611eac3102da75541fb7c8
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2026-01-05 19:15:08 +0530

    loader , useLoaderDats , routing

--- React/SocialMediaV1/social-media-3/src/components/Sidebar.jsx
+++ React/SocialMediaV1/social-media-3/src/components/Sidebar.jsx
@@ -1,10 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = ({ }) => {
 
-  const handleOnClick = (tabName) => {
-    setSelected(tabName)
-  }
+
   return (
     <>
       <div
